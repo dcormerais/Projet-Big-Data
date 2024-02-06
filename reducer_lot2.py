e46b3e1aff907d8bee3b5aa154eec73be6c0b148
Alfchristo <juppchristophe@hotmail.com>
2024-02-06 14:48:27 +0100

    - correction lot2

--- reducer_lot2.py
+++ reducer_lot2.py
@@ -49,19 +49,17 @@ for codcde, values in selection_aleatoire:
 
 # Données à mettre dans le DataFrame
 df = pd.DataFrame(
-    mydata, columns=["Code Commande", "Ville", "Nb. Articles sans Timbre", "Moyennes Quantités de la Commande"]
+    mydata, columns=["Code Commande", "Ville", "Nb. Articles sans Timbre", "Moyennes Quantites de la Commande"]
 )
 
 
 df.to_excel("/datavolume1/lot2_exo1.xlsx", index=False)
-print(df)
 
 #df = pd.read_excel("/Users/gueye/Documents/test/lot2_test.xlsx")
 
 
-plt.pie(df["Moyennes Quantités de la Commande"], labels=df["Ville"], autopct='%1.1f%%', startangle=90)
-plt.axis('equal')
-plt.title('Répartition des villes par rapport à la quantité')
+plt.pie(df["Moyennes Quantites de la Commande"], labels=df["Ville"], autopct='%1.1f%%', startangle=90)
+plt.title('Repartition des villes par rapport a la quantite Avec Hadoop')
 
 
 plt.savefig('/datavolume1/PieChart01.pdf')
